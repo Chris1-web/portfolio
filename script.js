5076ec16254f58e8e4e57b102cc9238bd8ba32f4
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-11-01 05:41:35 +0100

    create handburger menu

---
+++ script.js
@@ -0,0 +1,38 @@
+const handburgerMenuContainer = document.querySelector(".handburger-menu");
+const topMenu = document.querySelector(".top");
+const middleMenu = document.querySelector(".middle");
+const bottomMenu = document.querySelector(".bottom");
+const heroSection = document.querySelector(".hero-section");
+const logo = document.querySelector(".logo");
+const mobileNavContainer = document.querySelector(".mobile-nav-container");
+const mobileNavContainerLeft = document.querySelector(
+  ".mobile-nav-container.left"
+);
+const middleNav = document.querySelector(".middle-nav");
+const navContactBtn = document.querySelector("nav a.contact-button");
+
+const openAndCloseMenu = function () {
+  topMenu.classList.toggle("down");
+  middleMenu.classList.toggle("down");
+  bottomMenu.classList.toggle("down");
+  setTimeout(() => {
+    topMenu.classList.toggle("bend");
+    middleMenu.classList.toggle("bend");
+    bottomMenu.classList.toggle("bend");
+    mobileNavContainer.classList.toggle("show");
+    mobileNavContainerLeft.classList.toggle("show");
+    middleNav.classList.toggle("show");
+    navContactBtn.classList.toggle("show");
+  }, 500);
+};
+
+handburgerMenuContainer.addEventListener("click", openAndCloseMenu);
+mobileNavContainer.addEventListener("click", openAndCloseMenu);
+mobileNavContainerLeft.addEventListener("click", openAndCloseMenu);
+
+middleNav.addEventListener("click", (e) => {
+  e.currentTarget.classList.contains("show") && openAndCloseMenu();
+});
+navContactBtn.addEventListener("click", (e) => {
+  e.currentTarget.classList.contains("show") && openAndCloseMenu();
+});
